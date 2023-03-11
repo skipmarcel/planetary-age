f6969ea3210c7d366ab8ce604588dcd63b124ce1
Tyler Adams <skipmarcel@gmail.com>
2023-03-11 07:07:21 -0800

    add red test for multiplying earth age

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -7,4 +7,10 @@ describe("PlanetAge", () => {
     const result = planetAge.compAge("mercury");
     expect(result).toBeCloseTo(expected);
   });
+  test("should correctly multiply result by ageInDays", () => {
+    const planetAge = new PlanetAge(48 * 365.25);
+    const expected = (365.25 / 87.97) * 48;
+    const result = planetAge.compAge("mercury");
+    expect(result).toBeCloseTo(expected);
+  });
 });
